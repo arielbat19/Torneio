7bff3d827203f62204b49763f62110fd06347d51
arielbat19 <arielbatista853@gmail.com>
2025-04-29 15:33:52 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/com/teste/pratico/desafios/Desafio/Tecnico/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.pratico.desafios.Desafio.Tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
